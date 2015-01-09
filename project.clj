6d87fc319d2f3cdd41a9cdd649fa2e9a76a72c99
南山 <junjiemars@gmail.com>
2015-01-09 17:24:50 +0800

    init commit

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject books "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject books "0.1.0"
+  :description "my books"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.1"]
+                 [ring/ring-jetty-adapter "1.3.1"]
+                 ])
