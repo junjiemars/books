c4e2076feecdfd213d2810f1419a4e7d50755fa2
南山 <junjiemars@gmail.com>
2015-01-12 16:07:03 +0800

    raw ring demo

--- project.clj
+++ project.clj
@@ -6,4 +6,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.1"]
                  [ring/ring-jetty-adapter "1.3.1"]
-                 ])
+                 ]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler owl.books.core/handler
+         :init owl.books.core/init
+         :destroy owl.books.core/destroy
+         #_ (:nrepl {:start? true
+                     :port 2999})})
--- src/owl/books/core.clj
+++ src/owl/books/core.clj
@@ -1,6 +1,24 @@
-(ns books.core)
+(ns owl.books.core
+  (:require [ring.util.response :as rur])
+  (:require [ring.middleware
+             [file :as rmf]
+             [resource :as rmr]
+             [not-modified :as rmn]
+             [content-type :as rmc]
+             [session :as rms]]))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello, my books!"})
+   :body (list request)})
+
+(defn init []
+  (println "#init lein ring..."))
+
+(defn destroy []
+  (println "#destroy lein ring..."))
+
+(def app
+  (-> (fn [{session :session}]
+        (rur/response (str "abc " (:username session))))
+      (rms/wrap-session)))
